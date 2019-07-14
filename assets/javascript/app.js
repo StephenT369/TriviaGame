374f4bc5d013836156e21c6f2ebb35b32912ff45
Stephen Thompson <smt3@hotmail.com>
2019-07-14 11:51:55 -0500

    added test questions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,23 +1,47 @@
 window.onload = function(){
     $('#start').on('click', countDn)
+    $('#start').on('click', question)
 };
 
 var intervalId;
-
 var countRunning = false;
 var time = 21;
+var questions = [
+    {
+        question: 'What is 1+1?',
+        options: ['2', '3', '4'],
+        answer: 0
+    },
+    
+    {
+        question: 'What is 2+2?',
+        options: ['0', '4', '6'],
+        answer: 1
+    },
+
+    {
+        question: 'What is 3+3?',
+        options: ['4', '5', '6'],
+        answer: 2    
+    }
+];
+
 
 function countDn(){
     clearInterval(intervalId);
     $('#start').remove();
     $('#timeCntr').text('21');
-    intervalId = setInterval(count, 3000);
+    intervalId = setInterval(count, 1000);
     $('#timeRm').append('Time Remaining: ');
     $('#secs').append(' Seconds');
     if (!countRunning){
 
     };
 
+};
+
+function question(){
+    $('#question').text('test question')
 };
 
 function count(){
