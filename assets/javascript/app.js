a617f61e513477d49874949616fa7bc58269aa62
Stephen Thompson <smt3@hotmail.com>
2019-07-14 21:11:43 -0500

    added dynamic buttons

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,31 +1,48 @@
 window.onload = function(){
     $('#start').on('click', countDn)
-    $('#start').on('click', question)
+    //$('#start').on('click', question)
 };
 
 var intervalId;
 var countRunning = false;
 var time = 21;
+
 var questions = [
     {
         question: 'What is 1+1?',
         options: ['2', '3', '4'],
-        answer: 0
+        answer: 2
     },
     
     {
         question: 'What is 2+2?',
         options: ['0', '4', '6'],
-        answer: 1
+        answer: 4
     },
 
     {
         question: 'What is 3+3?',
         options: ['4', '5', '6'],
-        answer: 2    
+        answer: 6    
     }
 ];
 
+var questionsTrack = questions.length;
+
+var showQuestionOne = function(){
+    var ans = questions[0].answer;
+    console.log(ans);
+    $('#question').text(questions[0].question);
+
+    for (var i = 0; i < questions[0].options.length; i++){
+            var btn = $('<button>');
+            var brTag = $('<br>');
+            btn.addClass('options');
+            btn.attr('answer', questions[0].options[i]);
+            btn.text(questions[0].options[i]);
+            $('#btn-options').append(btn).append(brTag);   
+    }
+};
 
 function countDn(){
     clearInterval(intervalId);
@@ -34,15 +51,13 @@ function countDn(){
     intervalId = setInterval(count, 1000);
     $('#timeRm').append('Time Remaining: ');
     $('#secs').append(' Seconds');
+    showQuestionOne();
     if (!countRunning){
-
     };
 
 };
 
-function question(){
-    $('#question').text('test question')
-};
+
 
 function count(){
 time--;
@@ -57,4 +72,5 @@ if (time <= 0){
 function stop(){
     clearInterval(intervalId);
     return countRunning;
-}
+};
+
